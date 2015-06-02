d3847ef49619d3dc3daabf23005735ba78ab777f
Zeb Pykosz <hello@zebpykosz.com>
2015-06-02 18:01:15 -0400

    add ajax logic

--- js/app.js
+++ js/app.js
@@ -8,11 +8,75 @@
 
 // userFeed.run();
 
-$(window).load(function() {
-    $('a').on('click', function() {
+(function() {
+
+    var scrollToHash = function(hash) {
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
+            scrollTop: $(hash).offset().top
         }, 500);
-        return false;
+    };
+
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var hash = $(this).attr('href');
+        scrollToHash(hash);
     });
-});
+
+    var $body = $('body');
+    var $modal = $('<div class="modal animated fadeInDown"></div>');
+
+    $('body').on('click', '[data-modal-close]', function(event) {
+        event.preventDefault();
+
+        $(this).closest('.modal').addClass('fadeOut').delay(1000).removeClass('fadeOut').remove();
+
+        $body.removeClass('noscroll');
+        scrollToHash('#work');
+        document.location.hash = '';
+    });
+
+    var openModal = function(data) {
+        $body.append($modal);
+
+        var $content = $(data).find('#project').html();
+
+        $modal.html($content);
+
+        $body.addClass('noscroll');
+    };
+
+    var getContent = function(url, cb) {
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(data) {
+                cb(data);
+            },
+            error: function(e) {
+                console.log(e);
+            }
+        });
+    };
+
+    var showProject = function(url) {
+        getContent(url, function(data) {
+            openModal(data);
+            document.location.hash = url;
+        });
+    };
+
+    $('.project-anchor').on('click', function(event) {
+
+        event.preventDefault();
+
+        var url = $(this).attr('href');
+
+        showProject(url);
+
+    });
+
+
+    // window.onhashchange = showProject(window.location.hash);
+
+})();
