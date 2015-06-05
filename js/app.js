971cdb58dc3528a4bba6cbcc30e9a1be8309d88b
Zeb Pykosz <hello@zebpykosz.com>
2015-06-04 22:42:21 -0400

    add ajax logic

--- js/app.js
+++ js/app.js
@@ -10,10 +10,12 @@
 
 (function() {
 
-    var scrollToHash = function(hash) {
-        $('html, body').animate({
+    var scrollToHash = function(hash, cb) {
+        $('html, body').stop(true, true).animate({
             scrollTop: $(hash).offset().top
-        }, 500);
+        }, 500, function() {
+            cb();
+        });
     };
 
     $('a[href^="#"]').on('click', function(e) {
@@ -22,5 +24,70 @@
         scrollToHash(hash);
     });
 
+    // project link clicked
+    var $projectLink = $('[data-project]').find('a');
+
+    var loadProject = function(url, cb) {
+
+        setTimeout(function() {
+
+            // load content via ajax
+            $.ajax({
+                type: 'get',
+                url: url,
+                // async: true,
+                success: function(data) {
+                    cb(data);
+                }
+            });
+
+        // REMOVE FOR PRODUCTION. just to dummy latency
+        }, 0);
+    };
+
+    $projectLink.on('click', function(event) {
+
+        event.preventDefault();
+
+        var $this = $(this);
+
+        var $project = $this.closest('[data-project]');
+        var $projectSummary = $project.find('.project-summary');
+
+        if($project.hasClass('project-loaded')) return false;
+
+        // disable links
+        $projectLink.attr('disabled', true);
+        
+        var url = $this.attr('href');
+
+        var projectId = $project.attr('id');
+        var projectHash = '#' + projectId;
+
+        // add loading to project container
+        $project.addClass('project-loading');
+
+        var $newContent = $('<div class="project-content"></div>');
+
+        // append new content
+        $project.append($newContent);
+
+        loadProject(url, function(data) {
+
+            $project.removeClass('project-loading').addClass('project-loaded');
+
+            scrollToHash(projectHash, function() {
+                
+                // hide content
+                $projectSummary.addClass('fadeOut');
+
+                var content = $(data).find('#content');
+
+                // show new content
+                $newContent.html(content);
+            });
+        });
+
+    });
 
 })();
