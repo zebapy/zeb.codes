1b6d167ff6ef112ab6bb99c4cf27eee0344d5029
Zeb Pykosz <hello@zebpykosz.com>
2015-06-06 18:10:18 -0400

    convert to jquery plugin

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// var userFeed = new Instafeed({
-//     get: 'user',
-//     userId: 259725889,
-//     accessToken: '259725889.467ede5.c033f82aca634bf2a615a1352f958de0',
-//     limit: 6,
-//     template: '<a href="{{link}}" data-likes="{{likes}}"><img src="{{image}}"></a>'
-// });
-
-// userFeed.run();
-
 (function() {
 
     var scrollToHash = function(hash, cb) {
@@ -18,111 +8,140 @@
         });
     };
 
+    var pluginName = 'projectify';
+    var defaults = {
+        projectLoadingClass: 'loading',
+        projectDoneClass: 'open'
+    };
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.id = this.$element.attr('id');
+        this.$projectContent = $('<div class="project-content"></div>');
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+
+        init: function() {
+            this.$element.data('loaded', false);
+            this.initListener();
+            this.$projectContent.hide();
+            this.$element.append(this.$projectContent);
+        },
+
+        initListener: function() {
+            var self = this;
+            var $projectLink = this.$element.find('a');
+
+            $projectLink.on('click', function(event) {
+                event.preventDefault();
+                self.onProjectLinkClick($projectLink);
+            });
+        },
+
+        onProjectLinkClick: function($link) {
+            var url = $link.attr('href');
+
+            var projectLoaded = this.$element.data('loaded');
+            var projectOpen = this.$element.hasClass(this.settings.projectDoneClass);
+
+            if(!projectLoaded) {
+                this.openProject(url);
+            } else if(projectLoaded && !projectOpen) {
+                this.showProjectContent();
+            } else {
+                this.closeProject();
+            }
+        },
+
+        openProject: function(url) {
+            var self = this;
+
+            this.onProjectLoading();
+
+            this.loadData(url, function(data) {
+
+                self.content = $(data).find('#content');
+
+                self.insertProjectContent();
+
+                self.scrollToProject(function() {
+
+                    self.onProjectLoaded();
+
+                    self.showProjectContent();
+
+                });
+
+            });
+        },
+
+        insertProjectContent: function() {
+            this.$projectContent.html(this.content);
+        },
+
+        showProjectContent: function() {
+            this.$element.addClass(this.settings.projectDoneClass);
+            this.$projectContent.show();
+        },
+
+        onProjectLoading: function() {
+            this.$element.addClass(this.settings.projectLoadingClass);
+        },
+
+        onProjectLoaded: function() {
+            this.$element.removeClass(this.settings.projectLoadingClass);
+            this.$element.data('loaded', true);
+        },
+
+        scrollToProject: function(cb) {
+            var hash = '#' + this.id;
+            scrollToHash(hash, cb);
+        },
+
+        closeProject: function() {
+            this.$projectContent.hide();
+            this.$element.removeClass(this.settings.projectDoneClass);
+        },
+
+        loadData: function(url, cb) {
+            setTimeout(function() {
+
+                $.ajax({
+                    type: 'get',
+                    url: url,
+                    success: function(data) {
+                        cb(data);
+                    },
+                    error: function() {
+
+                    }
+                });
+
+            // REMOVE FOR PRODUCTION. just to dummy latency
+            }, 1000);
+        }
+    });
+
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if(!$.data( this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+    $('[data-project]').projectify();
+
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
         var hash = $(this).attr('href');
         scrollToHash(hash);
     });
-
-    // project link clicked
-    var $projectLink = $('[data-project]').find('a');
-
-    var projectLoadingClass = 'loading';
-    var projectDoneClass = 'open';
-    var currentProject = '';
-
-    var getData = function(url, cb) {
-
-        setTimeout(function() {
-
-            // load content via ajax
-            $.ajax({
-                type: 'get',
-                url: url,
-                // async: true,
-                success: function(data) {
-                    cb(data);
-                }
-            });
-
-        // REMOVE FOR PRODUCTION. just to dummy latency
-        }, 1000);
-    };
-
-    var showProjectContent = function($project) {
-
-    };
-
-    var hideProjectContent = function() {
-
-    };
-
-    var closeProject = function(id) {
-
-        var $projectToClose = $(id);
-
-        var $projectContent = $projectToClose.find('.project-content');
-
-        $projectToClose.removeClass(projectDoneClass);
-
-        $projectContent.slideUp();
-    };
-
-    var openProject = function($this) {
-
-        var url = $this.attr('href');
-
-        var $project = $this.closest('[data-project]');
-        var $projectSummary = $project.find('.project-summary');
-        var projectId = $project.attr('id');
-        var projectHash = '#' + projectId;
-        var $projectContent = $('<div class="project-content"></div>');
-
-        $projectContent.hide();
-        $project.append($projectContent);
-
-        if($project.hasClass(projectDoneClass)) {
-            return closeProject(projectHash);
-        } else if ($project.data('loaded') === true) {
-            return $projectContent.slideDown();
-        }
-
-        // disable links
-        $projectLink.attr('disabled', true);
-
-        currentProject = projectId;
-
-        // add loading to project container
-        $project.addClass(projectLoadingClass).attr('data-loaded', true);
-
-        // append new content
-        getData(url, function(data) {
-
-            $project.removeClass(projectLoadingClass).addClass(projectDoneClass);
-
-            scrollToHash(projectHash, function() {
-                
-                // hide content
-                $projectSummary.addClass('fadeOut');
-
-                var content = $(data).find('#content');
-
-                // show new content
-                $projectContent.html(content);
-
-                $projectContent.slideDown();
-            });
-        });
-    };
-
-    $projectLink.on('click', function(event) {
-
-        event.preventDefault();
-
-        openProject($(this));
-
-    });
-
-    
 
 })();
