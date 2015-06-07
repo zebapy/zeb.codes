786e73f207e46e16aca7e760a3afb3b0f96239ad
Zeb Pykosz <hello@zebpykosz.com>
2015-06-07 12:17:49 -0400

    add source maps

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,27 @@ var cmq = require('gulp-combine-mq');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('css', function() {
     return gulp.src('./_site/css/app.css')
+        .pipe(sourcemaps.init())
         .pipe(cmq())
         .pipe(minifyCss())
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('js', function() {
     return gulp.src('./_site/js/app.js')
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./js'));
 });
 
