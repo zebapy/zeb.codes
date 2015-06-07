e7e17b0c572825150a3c6d7e7b6eaef3b15588d0
Zeb Pykosz <hello@zebpykosz.com>
2015-06-06 20:13:48 -0400

    add ajax form functions

--- js/app.js
+++ js/app.js
@@ -144,4 +144,81 @@
         scrollToHash(hash);
     });
 
+    var addHasErrorToGroup = function($group) {
+        $group.parent('.form-group').addClass('has-error');
+    };
+
+    var validateEmail = function(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(email);
+    };
+
+    var $formSuccessContent = $('<h1>Your message has been sent. I\'ll get back to you as soon as I can :)</h1>');
+
+    $('form').submit(function(event) {
+
+        event.preventDefault();
+
+        var hasErrors = false;
+
+        var $form = $(this);
+        var $formGroup = $form.find('.form-group');
+        var $emailField = $form.find('input[name="email"]');
+        var $submitButton = $form.find('input[type="submit"]');
+        var originalSubmitButtonVal = $submitButton.val();
+        var gotcha = $form.find('input[name="_gotcha"]').val();
+        var name = $form.find('input[name="name"]').val();
+        var email = $emailField.val();
+        var message = $form.find('textarea[name="message"]').val();
+
+        $formGroup.removeClass('has-error');
+        $form.addClass('sending');
+        $submitButton.val('Sending message...').attr('disabled', true);
+        
+        if(!validateEmail(email)) {
+            addHasErrorToGroup($emailField);
+            hasErrors = true;
+        }
+
+        $form.find('[required]').each(function(index, field) {
+            var $field = $(field);
+            var val = $field.val();
+            if(!val) {
+                addHasErrorToGroup($field);
+                hasErrors = true;
+            }
+        });
+
+        if(hasErrors) {
+            $submitButton.val(originalSubmitButtonVal).attr('disabled', false);
+            return $form.removeClass('sending');
+        }
+
+        var formData = {
+            name: name,
+            email: email,
+            message: message,
+            _gotcha: gotcha
+        };
+
+        $.ajax({
+            url: '//formspree.io/zeb.apy@gmail.com', 
+            method: 'POST',
+            data: formData,
+            dataType: 'json',
+            success: function() {
+                $form.slideUp();
+                $formSuccessContent.hide();
+                $form.before($formSuccessContent);
+                $form.remove();
+                $formSuccessContent.fadeIn();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(arguments);
+            }
+        });
+    });
+
+    
+
 })();
