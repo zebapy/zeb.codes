a98f2ff89fbba801e07e5298d2e30d5a9a1bc385
Zeb Pykosz <zeb.apy@gmail.com>
2020-09-08 22:16:41 -0400

    add favicon back

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (config) {
   config.addPassthroughCopy('src/img');
   config.addPassthroughCopy('src/css');
   config.addPassthroughCopy('src/js');
+  config.addPassthroughCopy('favicon.ico');
 
   config.setBrowserSyncConfig({
     open: true
