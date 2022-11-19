b566219e19c7792e86cc7290ce97bb91aea8aa68
Zeb Pykosz <zeb.apy@gmail.com>
2022-11-19 11:01:39 -0500

    uses fixes

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -13,21 +13,23 @@ export default function UsesPage() {
         <div key={category} className="mb-8">
           <h2 className="h3 capitalize">{category}</h2>
           <ul className="list-disc pl-4 space-y-1">
-            {items.map((item) => (
-              <li key={item.name}>
-                {item.url ? (
-                  <a
-                    href={item.url}
-                    className="text-lg border-b hover:border-b-0"
-                  >
-                    {item.name}
-                  </a>
-                ) : (
-                  item.name
-                )}
-                <span className="text-gray-400 ml-2">{item.text}</span>
-              </li>
-            ))}
+            {items
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((item) => (
+                <li key={item.name}>
+                  {item.url ? (
+                    <a
+                      href={item.url}
+                      className="text-lg border-b hover:border-b-0"
+                    >
+                      {item.name}
+                    </a>
+                  ) : (
+                    item.name
+                  )}
+                  <span className="text-gray-400 ml-2">{item.text}</span>
+                </li>
+              ))}
           </ul>
         </div>
       ))}
