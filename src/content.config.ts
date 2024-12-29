8e74784068d766673d985a6b5a78e3d1d23a2275
Zeb Pykosz <zeb.apy@gmail.com>
2024-12-29 10:53:57 -0500

    wip

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,40 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const projects = defineCollection({
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    // description: z.string(),
+    // Transform string to Date object
+    // startDate: z.coerce.date(),
+    // endDate: z.coerce.date().optional(),
+    // heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const work = defineCollection({
+  loader: glob({ base: "./src/content/work", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    year: z.coerce.date(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, projects, work };
---
+++ src/content/projects/goodnames/index.md
@@ -0,0 +1,3 @@
+---
+title: goodnames
+---
